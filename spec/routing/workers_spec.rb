169ea82824ef00eb3ef14290f9170557c21b676d
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-04 12:12:40 +0300

    [commit-3] Tests: Workers route

---
+++ spec/routing/workers_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe '/workers routes' do
+  it 'routes to workers#index' do
+    # expect(get '/workers').to route_to(controller: 'workers', action: 'index')
+    aggregate_failures do
+      expect(get('/workers')).to route_to('workers#index')
+      expect(get('/workers?page[number]=3')).to route_to(
+        'workers#index', page: { 'number' => 3}
+      )
+    end
+  end
+end
