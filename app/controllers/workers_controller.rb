d7474e1126fc7d0921033d2775d2c146e18171a7
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-04 13:53:12 +0300

    [commit-5] Do Code: Workers Controller

---
+++ app/controllers/workers_controller.rb
@@ -0,0 +1,5 @@
+class WorkersController < ApplicationController
+  def index # need to be deleted when implement an #index in details
+    render json: {}
+  end
+end
--- spec/routing/workers_spec.rb
+++ spec/routing/workers_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe '/workers routes' do
     aggregate_failures do
       expect(get('/workers')).to route_to('workers#index')
       expect(get('/workers?page[number]=3')).to route_to(
-        'workers#index', page: { 'number' => 3}
+        'workers#index', page: { 'number' => '3'}
       )
     end
   end
