3dbbd5149ed497fd1116075498c1a43fdeccaacf
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-04 13:33:34 +0300

    [commit-4] Implement Code: Workers route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/workers', to: 'workers#index'
+  resources :workers, only: [:index]
 end
