91d2088aef9fbdea81f29b296fd85e64edb9bc7d
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-07 19:55:53 +0300

    [commit-14] Create ticket model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_03_144904) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_163655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "worker"
+    t.string "state"
+    t.string "worker_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/tickets.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :ticket do
+    title { "MyString" }
+    description { "MyString" }
+    worker { "MyString" }
+    state { "MyString" }
+    worker_name { "MyString" }
+    created_at { "MyString" }
+  end
+end
