9db8f7456e339503c44622a28b3a12ee1e24fddd
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-08 09:13:49 +0300

    [commit-16] Add tests: tickets collection route

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,38 +3,33 @@ require 'rails_helper'
 RSpec.describe Ticket, type: :model do
   describe "#validations" do
     it 'should validate presence of attributes' do
-      worker = build :ticket,
+      ticket = build :ticket,
                      title: nil,
                      description: nil,
                      worker: nil,
                      state: nil,
                      worker_name: nil
-        expect(worker).not_to be_valid
-        expect(worker.errors.messages[:title]).to include("can't be blank")
-        expect(worker.errors.messages[:description]).to include("can't be blank")
-        expect(worker.errors.messages[:worker]).to include("can't be blank")
-        expect(worker.errors.messages[:state]).to include("can't be blank")
-        expect(worker.errors.messages[:worker_name]).to include("can't be blank")
+        expect(ticket).not_to be_valid
+        expect(ticket.errors.messages[:title]).to include("can't be blank")
+        expect(ticket.errors.messages[:description]).to include("can't be blank")
+        expect(ticket.errors.messages[:worker]).to include("can't be blank")
+        expect(ticket.errors.messages[:state]).to include("can't be blank")
+        expect(ticket.errors.messages[:worker_name]).to include("can't be blank")
       end
 
-    # it 'is valid with valid attributes' do
-    #   worker = build :worker # , :active => "true"
-    #   # Restrictions:
-    #   min_str_length = 1
-    #   max_str_length = 20
-    #   min_age = 16
-    #   max_age = 60
-    #   roles_available = ["Manager", "Developer", "UI/UX Designer"]
-    #   active_states_available = %w[true false]
-    #
-    #   aggregate_failures do
-    #     expect(worker.last_name.length).to be_between(min_str_length, max_str_length).inclusive
-    #     expect(worker.first_name.length).to be_between(min_str_length, max_str_length).inclusive
-    #     expect(worker.age.to_i).to be_between(min_age, max_age).inclusive
-    #     expect(roles_available).to include(worker.role)
-    #     expect(active_states_available).to include(worker.active)
-    #   end
-    # end
+    it 'is valid with valid attributes' do
+      ticket = build :ticket, :state => ""
+
+      # Restrictions:
+      min_str_length = 1
+      max_str_length = 40
+      states_available = ["Manager", "Developer", "UI/UX Designer"]
+
+      aggregate_failures do
+        expect(ticket.title.length).to be_between(min_str_length, max_str_length).inclusive
+        expect(states_available).to include(ticket.state)
+      end
+    end
   end
 
 end
