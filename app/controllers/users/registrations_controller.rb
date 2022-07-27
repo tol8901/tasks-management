867d9c8bdb922069b2e104f2a5e9caf24969e2e5
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-27 11:35:27 +0300

    [commit-34] Transfer setup_worker into User model

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,27 +10,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def register_success
-    @worker = Worker.create( {
-                               first_name: current_user.name.split(' ')[0],
-                               last_name: current_user.name.split(' ')[1],
-                               age: 20,
-                               role: "Developer",
-                               active: true
-                             } )
-    if @worker.save
-      render json: {
-        message: 'Signed up successfully.',
-        user: current_user,
-        message: 'Worker created successfully.',
-        worker: @worker
-      }, status: :ok
-    else
-      render json: {
-        message: "Worker can not be saved",
-        message: "#{@worker.errors.full_messages}"
-      }
-    end
-
+    render json: {
+      message: 'Signed up successfully.',
+      user: current_user,
+      worker: current_user.worker
+    }, status: :ok
   end
 
   def register_failed
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :worker
+  after_create :setup_worker
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,4 +8,14 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
 
+  def setup_worker
+    Worker.create( {
+                               first_name: name.split(' ')[0],
+                               last_name: name.split(' ')[1],
+                               age: 100,
+                               role: "Developer",
+                               active: true,
+                               user_id: id
+                             } )
+  end
 end
